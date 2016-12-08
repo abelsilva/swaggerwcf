e049f7e0fa19661b964b7f9a3a2315ab034c50eb
Abel Silva <abel.silva@gmail.com>
2016-12-07 20:47:20 -0400

    bump to v0.1.17

--- src/SwaggerWcf/Properties/AssemblyInfo.cs
+++ src/SwaggerWcf/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.16")]
-[assembly: AssemblyFileVersion("0.1.16")]
-[assembly: AssemblyInformationalVersion("0.1.16")]
+[assembly: AssemblyVersion("0.1.17")]
+[assembly: AssemblyFileVersion("0.1.17")]
+[assembly: AssemblyInformationalVersion("0.1.17")]
