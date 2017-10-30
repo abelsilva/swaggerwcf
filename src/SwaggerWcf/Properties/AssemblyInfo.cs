8f2dbbe2dc00fcab49b94bb88f2223fc6ac8c673
Abel Silva <abel.silva@gmail.com>
2017-10-30 20:29:33 -0300

    release 0.2.1

--- src/SwaggerWcf/Properties/AssemblyInfo.cs
+++ src/SwaggerWcf/Properties/AssemblyInfo.cs
@@ -37,6 +37,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.29")]
-[assembly: AssemblyFileVersion("0.1.29")]
-[assembly: AssemblyInformationalVersion("0.1.29")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyInformationalVersion("0.2.1")]
