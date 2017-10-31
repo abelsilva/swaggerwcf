d70ba41be1fa749b4582c119c9a91f3604a9a54b
Abel Silva <abel.silva@gmail.com>
2017-10-31 17:42:28 -0300

    fixing #113

--- src/SwaggerWcf/Support/Mapper.cs
+++ src/SwaggerWcf/Support/Mapper.cs
@@ -233,6 +233,7 @@ namespace SwaggerWcf.Support
                 // remove duplicates
                 headers = headers.GroupBy(h => h.Name).Select(g => g.First()).ToList();
 
+                // parameters - headers
                 foreach (SwaggerWcfHeaderAttribute attr in headers)
                 {
                     operation.Parameters.Add(new ParameterPrimitive
@@ -246,7 +247,12 @@ namespace SwaggerWcf.Support
                     });
                 }
 
+                bool isGetRequest = httpMethod == "GET";
                 TypeBuilder typeBuilder = null;
+                if (!wrappedRequest && !isGetRequest && parameters.Length > 1)
+                {
+                    wrappedRequest = true;
+                }
                 if (wrappedRequest)
                 {
                     typeBuilder = new TypeBuilder(implementation.Name + "Request");
