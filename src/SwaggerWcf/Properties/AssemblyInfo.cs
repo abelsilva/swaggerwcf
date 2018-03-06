efbb79a534059aebc58bfd1fbabf8778b6d4d550
René Fehlow <rene@fehlow-it.de>
2018-03-06 14:25:12 +0100

    Nuget params changed

--- src/SwaggerWcf/Properties/AssemblyInfo.cs
+++ src/SwaggerWcf/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SwaggerWcf")]
+[assembly: AssemblyTitle("IwIn.SwaggerWcf")]
 [assembly: AssemblyDescription("Swagger for WCF")]
-[assembly: AssemblyCompany("abelsilva")]
-[assembly: AssemblyProduct("SwaggerWcf")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Immowelt AG")]
+[assembly: AssemblyProduct("IwIn.SwaggerWcf")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -37,6 +37,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.9")]
-[assembly: AssemblyFileVersion("0.2.9")]
-[assembly: AssemblyInformationalVersion("0.2.9")]
+[assembly: AssemblyVersion("1.0.0")]
