72a36b51d87cc27b11c56a18c68813d19a3ee103
Alexey Choporov <alexey.choporov@veezzo.com>
2018-04-10 10:02:02 +0200

    show Description attribute of enum member

--- src/SwaggerWcf/Support/DefinitionsBuilder.cs
+++ src/SwaggerWcf/Support/DefinitionsBuilder.cs
@@ -224,5 +224,19 @@ namespace SwaggerWcf.Support
             var val = Convert.ChangeType(enumVal, underlyingType);
             return Convert.ToInt32(val);
         }
+
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+
+            return "";
+        }
     }
 }
--- src/SwaggerWcf/Support/TypePropertiesProcessor.cs
+++ src/SwaggerWcf/Support/TypePropertiesProcessor.cs
@@ -136,9 +136,12 @@ namespace SwaggerWcf.Support
                 List<string> listOfEnumNames = propType.GetEnumNames().ToList();
                 foreach (string enumName in listOfEnumNames)
                 {
+                    var enumMemberItem = Enum.Parse(propType, enumName, true);
+                    string enumMemberDescription = DefinitionsBuilder.GetEnumDescription((Enum)enumMemberItem);
+                    enumMemberDescription = (string.IsNullOrWhiteSpace(enumMemberDescription)) ? "" : $"({enumMemberDescription})";
                     int enumMemberValue = DefinitionsBuilder.GetEnumMemberValue(propType, enumName);
                     if (prop.Description != null) prop.Enum.Add(enumMemberValue);
-                    enumDescription += "    " + enumName + System.Web.HttpUtility.HtmlEncode(" = ") + enumMemberValue + "\r\n";
+                    enumDescription += $"    {enumName}{System.Web.HttpUtility.HtmlEncode(" = ")}{enumMemberValue} {enumMemberDescription}\r\n";
                 }
 
                 if (enumDescription != "")
